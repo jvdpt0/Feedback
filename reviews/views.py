136987d69dda5d3f5ddce58b5d0a3baef94b5800
jvdpt0 <jvdpt0@gmail.com>
2023-11-21 03:40:51 -0300

    Adding class based views

--- reviews/views.py
+++ reviews/views.py
@@ -1,29 +1,39 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = 'reviews/reviews.html'
+    success_url = '/thank-you'
 
-        return render(request, 'reviews/review.html', {
-        'form': form,
-    })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        
-        return render(request, 'reviews/review.html', {
-        'form': form,
-    })
+class ThankYouView(TemplateView):
+   template_name='reviews/thank_you.html'
 
+   def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['message'] = 'This Works!'
+       return context
+   
+class ReviewListView(ListView):
+    template_name='reviews/reviews_list.html'
+    model = Review
+    context_object_name = 'reviews'
 
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html',)
+    
+class SingleReviewView(DetailView):
+    template_name = 'reviews/single_review.html'
+    model = Review
